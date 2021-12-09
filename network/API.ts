d4eb0eaa9f71bbbf5d50081f336222203f1ed6ce
Nguyen Hoang <hoang.nguyen@fruitful.io>
2021-12-09 19:32:21 +0700

    fix #3

--- network/API.ts
+++ network/API.ts
@@ -3,18 +3,35 @@ import Endpoints from '@network/Endpoint'
 
 const { get, post, put } = apiClient
 
-export const productList = (search: string, sort: string, page: number, record: number) =>
-get(`${Endpoints.PRODUCT}?search=${search}&sort=${sort}&page=${page}&record=${record}`)
+export const productList = (
+  search: string,
+  sort: string,
+  page: number,
+  record: number
+) =>
+  get(
+    `${Endpoints.PRODUCT}?search=${search}&sort=${sort}&page=${page}&record=${record}`
+  )
 export const productDetail = (productId: string) =>
-get(`${Endpoints.PRODUCT_DETAIL}?id=${productId}`)
+  get(`${Endpoints.PRODUCT_DETAIL}?id=${productId}`)
 export const productDetailSlug = (slug: string) =>
-get(`${Endpoints.PRODUCT_DETAIL}/slug?slug=${slug}`)
+  get(`${Endpoints.PRODUCT_DETAIL}/slug?slug=${slug}`)
 
 export const orderSubmit = (params: {}) => post(Endpoints.ORDER, params)
-export const orderDetail = (id:string) => get(`${Endpoints.ORDER}/detail?id=${id}`)
-export const getListOrder = (id:string) => get(`${Endpoints.ORDER,id}`);
-export const customerSignUp = (params: {}) => post(Endpoints.CUSTOMER_SIGNUP, params)
-export const customerLogIn = (params: {}) => post(Endpoints.CUSTOMER_LOGIN, params)
+export const orderDetail = (id: string) =>
+  get(`${Endpoints.ORDER}/detail?id=${id}`)
+export const getListOrder = () => get(Endpoints.ORDER)
+export const customerSignUp = (params: {}) =>
+  post(Endpoints.CUSTOMER_SIGNUP, params)
+export const customerLogIn = (params: {}) =>
+  post(Endpoints.CUSTOMER_LOGIN, params)
 
-export const categoryList = (search: string, sort: string, page: number, record: number) =>
-get(`${Endpoints.CATEGORY}?search=${search}&sort=${sort}&page=${page}&record=${record}`)
+export const categoryList = (
+  search: string,
+  sort: string,
+  page: number,
+  record: number
+) =>
+  get(
+    `${Endpoints.CATEGORY}?search=${search}&sort=${sort}&page=${page}&record=${record}`
+  )
